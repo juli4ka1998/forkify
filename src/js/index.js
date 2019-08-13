ee0d1a1470cefc892c691f17b050cac03e124c77
julia <juli4ka1998@gmail.com>
2019-08-13 13:57:57 +0300

    Making first API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-// Global app controller
+//
+//https://www.food2fork.com/api/search
+
+import axios from 'axios';
+
+async function getResults(query) {
+	const proxy = 'https://cors-anywhere.herokuapp.com/';
+	const key = '137ba01c55392775b560e8a0507c0fa3';
+	try {
+		const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+		const recipes = res.data.recipes;
+		console.log(recipes);
+	} catch (e) {
+		alert(e);
+	}
+}
+getResults('pizza');
